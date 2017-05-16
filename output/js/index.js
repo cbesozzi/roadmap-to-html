1124e93214896113996e7bf81e5451152218f0c5
ashleyvernon <avernon526@gmail.com>
2017-05-15 22:06:10 -0700

    adds conditional for navicon image

--- output/js/index.js
+++ output/js/index.js
@@ -2,4 +2,7 @@ $(function() {
   $("#js-toggle-menu").on("click", function() {
     $("body").toggleClass("menu-active");
   });
+  if (!Modernizr.svg) {
+  $("#menu").css("background-image", "url(navicon.png)");
+}
 });
