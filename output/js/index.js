a3317067453e3eb2a9961673774747892e55fb71
ashleyvernon <avernon526@gmail.com>
2017-05-15 22:12:57 -0700

    removes jquery for menu

--- output/js/index.js
+++ output/js/index.js
@@ -2,7 +2,4 @@ $(function() {
   $("#js-toggle-menu").on("click", function() {
     $("body").toggleClass("menu-active");
   });
-  if (!Modernizr.svg) {
-  $("#menu").css("background-image", "url(navicon.png)");
-}
 });
